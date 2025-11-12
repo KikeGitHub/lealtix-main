91a556eb9cfa604c3249764d7b3ae0d790a2faa5
KikeGitHub <kike2013edj@gmail.com>
2025-11-12 17:12:14 -0600

    feat: Add environment configurations for development and local setups

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://lealtix-service.onrender.com/api',
+  stripePublishableKey: 'pk_test_51S88J5F30XYgfrgVDNixlIFUVK9HphUlvSTFcG6IuoAheSQL2NBY95v9Zqy8xUY4WdREAJwOUW3FP3QmgsYwCq9400y8Ov9b6x'
+};
---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // Local environment (same as current default env)
+  apiUrl: 'http://localhost:8080/api',
+  stripePublishableKey: 'pk_test_51S88J5F30XYgfrgVDNixlIFUVK9HphUlvSTFcG6IuoAheSQL2NBY95v9Zqy8xUY4WdREAJwOUW3FP3QmgsYwCq9400y8Ov9b6x'
+};
